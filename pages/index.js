f30a8449eb2a3e8d1eabf5d3851e6e367e833c0b
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-11-29 16:47:36 +0200

    Back to zero

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,5 @@
 import styles from '../styles/Home.module.scss'
-import Link from 'next/link'
-import { useState } from 'react'
 
 export default function Home() {
-  const [activeIndex, setActiveIndex] = useState(null)
-
-  return (
-    <main className={styles.menu}>
-      <section>
-        <Link href='#' onMouseOver={() => setActiveIndex(0)}>
-          Work
-        </Link>
-
-        <Link href='#' onMouseOver={() => setActiveIndex(1)}>
-          Resume
-        </Link>
-
-        <Link href='#' onMouseOver={() => setActiveIndex(2)}>
-          About
-        </Link>
-
-        <Link href='#' onMouseOver={() => setActiveIndex(3)}>
-          Contact
-        </Link>
-      </section>
-
-      <div data-active-index={activeIndex} id={styles.menuBkPatern}></div>
-      <div data-active-index={activeIndex} id={styles.bkImage}></div>
-
-      <section>
-        <h1>Hello</h1>
-      </section>
-    </main>
-  )
+  return <main>Homepage</main>
 }
