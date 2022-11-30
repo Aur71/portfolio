bb1885135061f2d484db827e07133d0aaeddf3a8
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-11-30 15:24:50 +0200

    design almost done

--- pages/work.jsx
+++ pages/work.jsx
@@ -4,7 +4,14 @@ const work = () => {
   return (
     <section className={styles.work}>
       <div className={styles.gridContainer}>
-        <div className={styles.gridItem}></div>
+        <div className={styles.gridItem}>
+          <div className={styles.btnContainer}>
+            <a href='#'>live</a>
+            <a href='#'>code</a>
+            <button>about</button>
+          </div>
+        </div>
+
         <div className={styles.gridItem}></div>
         <div className={styles.gridItem}></div>
         <div className={styles.gridItem}></div>
