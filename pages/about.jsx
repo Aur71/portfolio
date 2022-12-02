da0dc825a1b22361b5001249d467d4cefe88850b
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-12-02 10:47:38 +0200

    Contact page done

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,43 @@
 import styles from '../styles/About.module.scss'
+import { useGlobalContext } from '../context/GlobalContext'
 
 const about = () => {
-  return <section className={styles.about}>about</section>
+  const { hoverActive, hoverIndex } = useGlobalContext()
+
+  return (
+    <section className={styles.about}>
+      <h2>About me</h2>
+      <div className={styles.paragraphs}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          accusamus nisi voluptatum labore expedita vel harum, corporis dolores
+          tempora asperiores recusandae in sunt vero eveniet!
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic itaque
+          placeat quos maxime sint exercitationem!
+        </p>
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+          repellat?
+        </p>
+      </div>
+
+      <div
+        data-hover={hoverActive}
+        data-index={hoverIndex}
+        id={styles.bkPatern}
+      ></div>
+
+      <div
+        data-hover={hoverActive}
+        data-index={hoverIndex}
+        id={styles.bkImage}
+      ></div>
+    </section>
+  )
 }
 
 export default about
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,7 +1,97 @@
 import styles from '../styles/Contact.module.scss'
+import { useState, useRef } from 'react'
+import emailjs from '@emailjs/browser'
 
 const contact = () => {
-  return <div className={styles.contact}>contact</div>
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_4rxply3',
+        'template_q5hwy3d',
+        form.current,
+        '-UVdtggYeFOqoYISa'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+          window.alert(`Message sent`)
+        },
+        (error) => {
+          console.log(error.text)
+          window.alert(`Error: ${error}`)
+        }
+      )
+
+    setName('')
+    setEmail('')
+    setMessage('')
+  }
+
+  return (
+    <div className={styles.contact}>
+      <h2>Contact me</h2>
+
+      <form onSubmit={sendEmail} ref={form}>
+        <div>
+          <div className={styles.nameWrapper}>
+            <input
+              type='text'
+              placeholder='Name'
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              name='from_name'
+            />
+
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+
+          <div className={styles.emailWrapper}>
+            <input
+              type='email'
+              placeholder='Email'
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              name='email_id'
+            />
+
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <div className={styles.messageWrapper}>
+          <textarea
+            name='message'
+            placeholder='Message'
+            required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <button type='submit'>Send</button>
+      </form>
+    </div>
+  )
 }
 
 export default contact
