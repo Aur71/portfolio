25f1e1f7f90d76c520520f944f3845008dc2a835
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-12-04 14:02:32 +0200

    animation changes

--- pages/_app.js
+++ pages/_app.js
@@ -7,31 +7,36 @@ import { useRouter } from 'next/router'
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
 
+  const animation = {
+    key: router.route,
+
+    transition: {
+      duration: 0.5,
+      ease: "easeOut"
+    },
+
+    initial: {
+      opacity: 0,
+      translateX: 100
+    },
+    animate: {
+      opacity: 1,
+      translateX: 0
+    },
+    exit: {
+      opacity: 0,
+      translateX: -100,
+      transition: {
+        duration: 0.2,
+        ease: 'easeOut'
+      }
+    },
+  }
+
   return (
     <AppProvider>
       <AnimatePresence exitBeforeEnter>
-        <motion.div
-          key={router.route}
-          initial='initialState'
-          animate='animateState'
-          exit='exitState'
-          transition={{
-            duration: 0.75,
-          }}
-          variants={{
-            initialState: {
-              opacity: 0,
-              clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
-            },
-            animateState: {
-              opacity: 1,
-              clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
-            },
-            exitState: {
-              clipPath: 'polygon(50% 0, 50% 0, 50% 100%, 50% 100%)',
-            },
-          }}
-        >
+        <motion.div {...animation}>
           <Layout>
             <Component {...pageProps} />
           </Layout>
