8cb7dd0155d0f5fdbfb44b37babac6b865044e59
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-12-05 11:59:25 +0200

    Page transition animation removed becouse of a bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,43 +1,14 @@
 import '../styles/globals.scss'
 import Layout from '../components/Layout'
 import { AppProvider } from '../context/GlobalContext'
-import { AnimatePresence, motion } from 'framer-motion'
-import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
-  const animation = {
-    key: router.route,
-
-    transition: {
-      duration: 0.1,
-      ease: "easeOut"
-    },
-
-    initial: {
-      opacity: 0,
-    },
-    animate: {
-      opacity: 1,
-    },
-    exit: {
-      opacity: 0,
-      transition: {
-        ease: 'easeOut'
-      }
-    },
-  }
 
   return (
     <AppProvider>
-      <AnimatePresence exitBeforeEnter>
-        <motion.div {...animation}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </motion.div>
-      </AnimatePresence>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </AppProvider>
   )
 }
